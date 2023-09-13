110ea6fc48c2ea624d081a61b4ff2a5f0f5cbbc6
CitrineQED <121299677+CitrineQED@users.noreply.github.com>
2023-09-14 06:05:56 +0900

    Add files via upload

---
+++ makeWAVdata.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include "wave.hpp"
+using namespace std;
+
+double r = pow(2.0, 1.0/12.0);
+double A_4 = 440.0; // A4(69) = 440Hz
+int n = 3;
+int main(){
+    wave sinwave;
+    sinwave.fs = 44100;
+    sinwave.bits = 16;
+    sinwave.L = sinwave.fs * sinwave.bits * 1 / 8  * 6 / 2;
+    double freqs[n] = {72,76,79};
+    sinwave.data = new double[sinwave.L];
+    for (int i = 0; i < sinwave.L; ++i){
+        sinwave.data[i] = 0.0;
+    }
+    for (int i = 0; i < sinwave.L; ++i){
+        for (int j = 0; j < 2; ++j){
+            sinwave.data[i] += sin(2.0 * 3.141592653589 / (double)sinwave.fs * (A_4 * pow(r,freqs[j]-69)) * (double)i) / (double)n;
+        }
+    }
+    sinwave.audio_write("sinmix.wav");
+}
